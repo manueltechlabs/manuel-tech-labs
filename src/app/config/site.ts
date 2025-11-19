0ff209d9921d861d5d95607cc55b683959c142a7
Manuel <nhernandez80@gmail.com>
2025-11-19 18:14:31 +0000

    commit 10dth improved icons from www.svgrepo.com

--- src/app/config/site.ts
+++ src/app/config/site.ts
@@ -47,19 +47,19 @@ export const siteConfig = {
     {
       title: 'Industrial & Supply Chain Engineering',
       description: 'Process modeling, operations research, KPI development, and physical system design.',
-      icon: '/icons/WhatIDo/factory.svg',
+      icon: '/icons/WhatIDo/settings-cogwheel-svgrepo-com.svg',
 
     },
     {
       title: 'Data Science & Analytics',
       description: 'Machine Learning (Python, R, PyTorch), SQL (MSSQL, HanaSQL), data visualization (Tableau), and statistical analysis.',
-      icon: '/icons/WhatIDo/brain.svg',
+      icon: '/icons/WhatIDo/brain-svgrepo-com.svg',
 
     },
     {
       title: 'Full-Stack Development',
       description: 'Java (Spring Boot), React, API Design, Databases (MySQL), CI/CD, and Cloud (AWS, Docker).',
-      icon: '/icons/WhatIDo/coding.svg',
+      icon: '/icons/WhatIDo/code-svgrepo-com.svg',
 
     },
   ],
